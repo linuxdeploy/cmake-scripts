df329871d82b9bbce76419aea59f595078dee024
TheAssassin <theassassin@assassinate-you.net>
2021-12-01 03:49:10 +0100

    Run pkg-config only when needed

--- include-or-build-gtest.cmake
+++ include-or-build-gtest.cmake
@@ -9,11 +9,11 @@ set(prefix __ld_gtest)
 # 1.11.0 introduced various matchers that can be used with EXPECT_THAT (e.g., ThrowsMessage)
 set(required_version 1.11.0)
 
-pkg_check_modules(${prefix} gtest>=${required_version} IMPORTED_TARGET)
-
 if(TARGET gtest)
     __cmake_scripts_log("target gtest found, not setting up again")
 else()
+    pkg_check_modules(${prefix} gtest>=${required_version} IMPORTED_TARGET)
+
     if(${prefix}_FOUND)
         __cmake_scripts_log("Using system gtest")
         add_library(gtest ALIAS PkgConfig::${prefix})
